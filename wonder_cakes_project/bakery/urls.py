4954a385b822e224f6046230f0ea8d2bbb518690
joelmutiso <joelwmutiso@gmail.com>
2025-08-26 04:13:31 +0300

    Delete wonder_cakes_project directory

--- wonder_cakes_project/bakery/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductListCreateView, ProductDetailView
-
-urlpatterns = [
-    path('products/', ProductListCreateView.as_view()),
-    path('products/<int:pk>/', ProductDetailView.as_view()),
-]
