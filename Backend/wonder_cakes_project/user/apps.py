2ab01d117ae7d8baae301f971031f8df9d08268f
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-30 17:33:11 +0300

    fixed endpoints for user registration, login, profile retrieval, and logout

--- Backend/wonder_cakes_project/user/apps.py
+++ Backend/wonder_cakes_project/user/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-class UserConfig(AppConfig):
+class UserConfig(AppConfig): # UserConfig for user-related settings
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
